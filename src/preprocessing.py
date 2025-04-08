2e5e6a1c23be3d2dc23b2dacaf91eeafaf6f3c84
Dexter Corley <dexter.corley@live.com>
2025-04-07 21:29:56 -0700

    lab 3 submission

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,11 @@
-train = pd.read_csv('../data/phone_prices/train.csv')
-test = pd.read_csv('../data/phone_prices/train.csv')
+import pandas as pd
+
+train = pd.read_csv('data/phone_prices/train.csv')
+# test = pd.read_csv('data/phone_prices/test.csv')
 
 y = train["price_range"]
 X = train.drop("price_range", axis=1)
 
-y_test = test["price_range"]
-X_test = test.drop("price_range", axis=1)
+# Save the processed data to new CSV files
+X.to_csv('data/phone_prices/train.csv', index=False)
+y.to_csv('data/phone_prices/y_train.csv', index=False)
